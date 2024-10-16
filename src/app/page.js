40fdac477dff6e3ac0a4964540154556af257882
akhlaqueahmad119 <akhlaqueahmad119@gmail.com>
2024-10-17 00:57:57 +0530

    fixed bug

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function HomePage() {
       <h1 className={styles.heading}>Welcome to the Product App</h1>
       <p className={styles.paragraph}>
         Search for products by navigating to{" "}
-        <code className={styles.code}>/product/[id]</code>
+        <code className={styles.code}>/product/id</code>
       </p>
     </div>
   );
